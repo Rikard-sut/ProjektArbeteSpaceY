dd3194b28ce06a401c55abfdf1fde35eecb92a47
Carl Svahn <calle_tag@hotmail.com>
2020-06-01 15:05:23 +0200

    Added Expedition model

---
+++ SpaceyWebbApp/Models/Expedition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceyWebbApp.Models
+{
+    public class Expedition
+    {
+        public int ExpeditionId { get; set; }
+        public DateTime Departure { get; set; }
+        public int RocketId { get; set; }
+        public int DestinationId { get; set; }
+        public FlightLength flightLength { get; set; }
+
+    }
+
+public enum FlightLength
+{
+    Low = 1, Medium = 2, High = 3
+}
+}
