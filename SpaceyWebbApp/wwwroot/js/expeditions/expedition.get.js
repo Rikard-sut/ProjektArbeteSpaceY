7dcb529b7ec9b25c5c9462480f0619237139abb3
Rikard-Sut <mailarikardskoglundnu@gmail.com>
2020-06-04 14:10:40 +0200

    Timeformat in expeditonsview

--- SpaceyWebbApp/wwwroot/js/expeditions/expedition.get.js
+++ SpaceyWebbApp/wwwroot/js/expeditions/expedition.get.js
@@ -10,8 +10,10 @@
 
 function populateTable(expeditions) {
     for (const expedition of expeditions) {
+        let departure = expedition.departure;
+        departure = departure.replace('T', ' ');
         const bookedSeats = expedition.customers.length
-        var markup = "<tr class='table-row' data-href='/admin/expedition/?id=" + expedition.expeditionId + "'><td>" + expedition.departure + "</td><td>" + expedition.destination.name + "</td><td>" + expedition.rocket.name + "</td><td>" + bookedSeats + "/" + expedition.rocket.seats + "</td> </tr> ";
+        var markup = "<tr class='table-row' data-href='/admin/expedition/?id=" + expedition.expeditionId + "'><td>" + departure + "</td><td>" + expedition.destination.name + "</td><td>" + expedition.rocket.name + "</td><td>" + bookedSeats + "/" + expedition.rocket.seats + "</td> </tr> ";
         $('.table').append(markup)
     }
     addListener();
