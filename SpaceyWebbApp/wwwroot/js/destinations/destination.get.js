558adcc39b1daffe8a2c30bfc56b023ba6aae963
Rikard-Sut <mailarikardskoglundnu@gmail.com>
2020-06-05 10:27:18 +0200

    Fixed lists at rocket and destinatins

---
+++ SpaceyWebbApp/wwwroot/js/destinations/destination.get.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    var jqxhr = $.get('/api/destinations')
+        .done(() => {
+            populateTable(jqxhr.responseJSON)
+        })
+        .fail(() => {
+
+        })
+})
+
+function populateTable(destinations) {
+    for (const destination of destinations) {
+        var markup = "<tr><td>" + destination.name + "</td><td>" + destination.expeditionRange + "</td></tr>";
+        $('.table').append(markup)
+    }
+}
